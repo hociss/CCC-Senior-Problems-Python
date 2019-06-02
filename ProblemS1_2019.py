4025674827a254afafff569550c4eb2ad68a1d0f
hociss <43524113+hociss@users.noreply.github.com>
2019-06-02 12:04:22 -0400

    Update ProblemS1_2019.py

--- ProblemS1_2019.py
+++ ProblemS1_2019.py
@@ -4,24 +4,24 @@
 flip = input()
 board = [1, 2, 3, 4]
 
-print(len(flip))
-
-for i in range (len(flip)):
+if flip.count('H')%2 != 0:
     p1 = board[0]
     p2 = board[1]
     p3 = board[2]
     p4 = board[3]
-
-    if (flip[i] == "H"):
-        board[0] = p3
-        board[2] = p1
-        board[1] = p4
-        board[3] = p2
-    elif (flip[i] == "V"):
-        board[0] = p2
-        board[1] = p1
-        board[2] = p4
-        board[3] = p3
+    board[0] = p3
+    board[2] = p1
+    board[1] = p4
+    board[3] = p2
+if flip.count('V')%2 != 0:
+    p1 = board[0]
+    p2 = board[1]
+    p3 = board[2]
+    p4 = board[3]
+    board[0] = p2
+    board[1] = p1
+    board[2] = p4
+    board[3] = p3
 
 print (board[0], board[1])
 print (board[2], board[3])
